bd33726d5d2c64dcf50b2b187dd45c0b98aabf59
parveenhumm <136242007+parveenhumm@users.noreply.github.com>
2024-04-28 02:05:42 +0500

    Add files via upload
    currency converter

---
+++ main.ts
@@ -0,0 +1,35 @@
+import inquirer from "inquirer"
+ const currency :any = {
+  USD : 1 ,
+  EUR : 0.91 ,
+  GBP : 0.76 ,
+  INR : 74.57 ,
+  PKR : 280 ,
+ } ;
+ let userAnswer = await inquirer.prompt(
+    [
+        {
+          name : 'from' ,
+          message : "Enter From Currency" ,
+          type :  'list' ,
+          choices : ['USD' , 'EUR' , 'GBP' , 'INR' , 'PKR' ]
+        },
+        {
+          name :  'to' ,
+          message : "Enter to Currency" ,
+          type : 'list' ,
+          choices : ['USD' , 'EUR' , 'GBP' , 'INR' , 'PKR' ]
+        },
+        {
+          name : 'amount' ,
+          message : "Enter your Amount" ,
+          type : 'number',
+        }
+    ]
+);
+let fromAmount = currency[userAnswer.from]
+let toAmount = currency[userAnswer.to]
+let amount = userAnswer.amount ;
+let baseAmount = amount/fromAmount ;
+let convertedAmount = baseAmount*toAmount 
+console.log(convertedAmount);
